0e266651e2b019e758548a9ca07d6aeb1e6cf2db
Austin Tang <austinlt@usi.edu>
2021-10-18 01:29:57 -0700

    cloud save

---
+++ js/about.js
@@ -0,0 +1,73 @@
+const format = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var live = 0, achiev = 0, compl = 0, misc = 0, user = 0;
+
+function parseTweets(runkeeper_tweets) {
+	//Do not proceed if no tweets loaded
+	if(runkeeper_tweets === undefined) {
+		window.alert('No tweets returned');
+		return;
+	}
+
+	tweet_array = runkeeper_tweets.map(function(tweet) {
+		return new Tweet(tweet.text, tweet.created_at);
+	});
+	
+	for(let i = 0; i < tweet_array.length; i++) {
+		console.log(tweet_array[i].written);
+		if(tweet_array[i].source == 'live_event') {
+			live += 1;
+		}
+
+		else if(tweet_array[i].source == 'achievement') {
+			achiev += 1;
+		}
+
+		else if(tweet_array[i].source == 'completed_event') {
+			compl += 1;
+			if(tweet_array[i].written) {
+				user += 1;
+			}
+		}
+
+		else {
+			misc += 1;
+		}
+	}
+
+	console.log(math.round((compl / (tweet_array.length)) * 100).toFixed(2));
+
+	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
+	//It works correctly, your task is to update the text of the other tags in the HTML file!
+	document.getElementById('numberTweets').innerText = tweet_array.length;	
+	document.getElementById('firstDate').innerText = tweet_array[tweet_array.length - 1].time.toLocaleDateString(undefined, format);
+	document.getElementById('lastDate').innerText = tweet_array[0].time.toLocaleDateString(undefined, format);
+	
+	var complList = document.querySelectorAll('.completedEvents');
+	for(let i = 0; i < complList.length; i++) {
+		complList[i].innerHTML = compl;
+	}
+	var complPercent = (compl / tweet_array.length) * 100;
+	document.querySelector('.completedEventsPct').innerHTML = complPercent.toFixed(2) + '%';
+
+	document.querySelector('.liveEvents').innerHTML = live;
+	var livePercent = (live / tweet_array.length) * 100;
+	document.querySelector('.liveEventsPct').innerHTML = livePercent.toFixed(2) + '%';
+
+	document.querySelector('.achievements').innerHTML = achiev;
+	var achievPercent = (achiev / tweet_array.length) * 100;
+	document.querySelector('.achievementsPct').innerHTML = achievPercent.toFixed(2) + '%';
+
+	document.querySelector('.miscellaneous').innerHTML = misc;
+	var miscPercent = (misc / tweet_array.length) * 100;
+	document.querySelector('.miscellaneousPct').innerHTML = miscPercent.toFixed(2) + '%';
+
+	document.querySelector('.written').innerHTML = user;
+	var writtenPercent = (user / compl) * 100;
+	console.log(user);
+	document.querySelector('.writtenPct').innerHTML = writtenPercent.toFixed(2) + '%';
+}
+
+//Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function (event) {
+	loadSavedRunkeeperTweets().then(parseTweets);
+});
---
+++ ts/tweet.ts
@@ -0,0 +1,119 @@
+class Tweet {
+	private text:string;
+	time:Date;
+
+	constructor(tweet_text:string, tweet_time:string) {
+        this.text = tweet_text;
+		this.time = new Date(tweet_time);//, "ddd MMM D HH:mm:ss Z YYYY"
+	}
+
+	//returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
+    get source():string {
+        if(this.text.startsWith('Just completed') || this.text.startsWith('Just posted')) {
+            return 'completed_event';
+        }
+
+        else if(this.text.startsWith('Achieved')) {
+            return 'achievement';
+        }
+
+        else if(this.text.startsWith('Watch my')) {
+            return 'live_event';
+        }
+
+        else {
+            return 'miscellaneous';
+        }
+        //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
+    }
+
+    //returns a boolean, whether the text includes any content written by the person tweeting.
+    get written():boolean {
+        if(this.text.includes('- Tom')) {
+            return false;
+        }
+
+        else if(this.text.includes('- ')) {
+            return true;
+        }
+
+        else {
+            return false;
+        } 
+    }
+
+    get writtenText():string {
+        if(!this.written) {
+            var hasHttps = this.text.split('-');
+            var noHttps = hasHttps[1];
+            var end = noHttps.split('https');
+            var user = end[0].slice(1, end[0].length - 1);
+
+            return user;
+        }
+
+        else {
+            return " ";
+        }
+        //TODO: parse the written text from the tweet
+    }
+
+    get activityType():string {
+        if (this.source != 'completed_event') {
+            return "unknown";
+        }
+
+        else {
+            if(this.text.includes('mi')) {
+                let splitted = this.text.split('mi');
+                let temp = splitted[1].split(' ');
+                return temp[1];
+            }
+
+            else {
+                let splitted = this.text.split('km');
+                let temp = splitted[1].split(' ');
+                return temp[1];
+            }
+        }
+        //TODO: parse the activity type from the text of the tweet
+    }
+
+    get distance():number {
+        if(this.source != 'completed_event') {
+            return 0;
+        }
+
+        else {
+            if(this.text.includes('in ')) {
+                let splitted = this.text.split('in ');
+                let temp = splitted[0].split(' ');
+                let tempNum = parseInt(temp[temp.length - 2]);
+                return tempNum;
+            }
+
+            else if(this.text.includes('mi')) {
+                let splitted = this.text.split('mi');
+                let temp = splitted[0].split(' ');
+                let tempNum = parseInt(temp[temp.length - 2]);
+                return tempNum;
+            }
+
+            else {
+                let splitted = this.text.split('km');
+                let temp = splitted[0].split(' ');
+                let tempNum = parseInt(temp[temp.length - 2]);
+                let converted = tempNum / 1.609;
+                return converted;
+            }
+
+
+        }
+        //TODO: prase the distance from the text of the tweet
+    }
+
+    getHTMLTableRow(rowNumber:number):string {
+        //TODO: return a table row which summarizes the tweet with a clickable link to the RunKeeper activity
+        return "<tr></tr>";
+    }
+}
